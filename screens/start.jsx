bcff25d516701cc6db5914610c86646514354f09
doudou <dushuying1996@gmail.com>
2024-09-21 16:36:27 -0700

    Validate name input for length and numerics: ensure the name is more than one character and contains no numbers

--- screens/start.jsx
+++ screens/start.jsx
@@ -1,13 +1,33 @@
-import { View, Text, TextInput, SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, SafeAreaView, Button } from 'react-native'
+import React, { useState } from 'react'
 import Checkbox from 'expo-checkbox'
 
 export default function start() {
+    const [name, setName] = useState('')
+    const [nameError, setNameError] = useState('')
+
+    const handleNameChage = (text) => {
+        const characters = text.split('');
+        const containsDigit = characters.some(char => '0123456789'.includes(char));
+
+        if (text.lenghth <= 1 || containsDigit) {
+            setNameError('Please enter a valid name');
+        } else {
+            setNameError('');
+        }
+        setName(text);
+    };
+
     return (
         <SafeAreaView>
             <View>
                 <Text>Name:</Text>
-                <TextInput />
+                <TextInput
+                    placeholder=''
+                    value={name}
+                    onChangeText={handleNameChage}
+                />
+                {nameError ? <Text>{nameError}</Text> : null}
             </View>
 
             <View>
