19acfa87504dea8e063ce2ee575a3a44269371ae
doudou <dushuying1996@gmail.com>
2024-09-21 20:42:59 -0700

    validate email address: ensure it only contains one "@", "@" is always before ".com" and ".com" is always at the end of the email address

--- screens/start.jsx
+++ screens/start.jsx
@@ -5,17 +5,31 @@ import Checkbox from 'expo-checkbox'
 export default function start() {
     const [name, setName] = useState('')
     const [nameError, setNameError] = useState('')
+    const [address, setAddress] = useState('')
+    const [addressError, setAddressError] = useState('')
 
-    const handleNameChage = (text) => {
+    const handleNameChange = (text) => {
         const characters = text.split('');
         const containsDigit = characters.some(char => '0123456789'.includes(char));
 
-        if (text.lenghth <= 1 || containsDigit) {
+        if (text.length <= 1 || containsDigit) {
             setNameError('Please enter a valid name');
         } else {
             setNameError('');
         }
         setName(text);
+    };
+
+    const handleEmailChange = (email) => {
+        const atIndex = email.indexOf('@');
+        const lastAtIndex = email.lastIndexOf('@');
+        const dotComIndex = email.lastIndexOf('.com');
+        if (atIndex === lastAtIndex && atIndex > 0 && dotComIndex > atIndex + 1 && dotComIndex === email.length - 4) {
+            setAddressError('');
+        } else {
+            setAddressError('Please enter a valid email address');
+        }
+        setAddress(email);
     };
 
     return (
@@ -25,14 +39,19 @@ export default function start() {
                 <TextInput
                     placeholder=''
                     value={name}
-                    onChangeText={handleNameChage}
+                    onChangeText={handleNameChange}
                 />
                 {nameError ? <Text>{nameError}</Text> : null}
             </View>
 
             <View>
                 <Text>Email Address:</Text>
-                <TextInput />
+                <TextInput
+                    placeholder=''
+                    value={address}
+                    onChangeText={handleEmailChange}
+                />
+                {addressError ? <Text>{addressError}</Text> : null}
             </View>
 
             <View>
