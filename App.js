b4e02f93ab23c3b23f926ab1f0977a1fc06ddc27
doudou <dushuying1996@gmail.com>
2024-09-25 10:39:43 -0700

    implement conditional rendering to determine which screen to switch to

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import Start from './screens/Start';;
+import { SafeAreaView, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import Start from './screens/Start';
+import Confirm from './screens/Confirm';
+import Game from './screens/Game';
 
 
 export default function App() {
+  const [currentScreen, setCurrentScreen] = useState('START');
+
+  const renderScree = () => {
+    switch (currentScreen) {
+      case 'START':
+        return <Start onRegister={() => setCurrentScreen('CONFIRM')} />;
+      case 'CONFIRM':
+        return <Confirm onConfirm={() => setCurrentScreen('GAME')}
+          onBack={() => setCurrentScreen('START')} />;
+      case 'GAME':
+        return <Game onEnd={() => setCurrentScreen('START')} />;
+      default:
+        return <Start onRegister={() => setCurrentScreen('CONFIRM')} />
+    }
+  }
+
   return (
     <SafeAreaView style={styles.container}>
+      {renderScree()}
       <StatusBar style="auto" />
-      <Start />
     </SafeAreaView>
   );
 }
@@ -16,7 +34,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
