a2fc5e24e52fd45cf4f287fef7b1554369c59a7d
doudou <dushuying1996@gmail.com>
2024-09-25 09:40:42 -0700

    import Start component to the App.js

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import start from './screens/Start';;
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <Start />
+    </SafeAreaView>
   );
 }
 
